81812c486e414d0e706c99cb1746303b89470544
sergey <zak_on@list.ru>
2025-02-03 23:06:06 +0300

    feature/chapter2.3

--- taco-cloud/taco-cloud/src/main/java/sia/tacocloud/converter/IngredientByIdConverter.java
+++ taco-cloud/taco-cloud/src/main/java/sia/tacocloud/converter/IngredientByIdConverter.java
@@ -4,6 +4,7 @@ package sia.tacocloud.converter;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 import sia.tacocloud.domain.entyty.Ingredient;
+import sia.tacocloud.domain.entyty.Ingredient.Type;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,16 +15,16 @@ public class IngredientByIdConverter implements Converter<String, Ingredient> {
     private Map<String, Ingredient> ingredientMap = new HashMap<>();
 
     public IngredientByIdConverter() {
-        ingredientMap.put("FLTO", new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP));
-        ingredientMap.put("COTO", new Ingredient("COTO", "Corn Tortilla", Ingredient.Type.WRAP));
-        ingredientMap.put("CRBF", new Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN));
-        ingredientMap.put("CARN", new Ingredient("CARN", "Carnitas", Ingredient.Type.PROTEIN));
-        ingredientMap.put("TMTO", new Ingredient("TMTO", "Diced Tomatoes", Ingredient.Type.VEGGIES));
-        ingredientMap.put("LETC", new Ingredient("LETC", "Lettuce", Ingredient.Type.VEGGIES));
-        ingredientMap.put("CHED", new Ingredient("CHED", "Cheddar", Ingredient.Type.CHEESE));
-        ingredientMap.put("JACK", new Ingredient("JACK", "Monterry Jack", Ingredient.Type.CHEESE));
-        ingredientMap.put("SLSA", new Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE));
-        ingredientMap.put("SRCR", new Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE));
+        ingredientMap.put("FLTO", new Ingredient("FLTO", "Flour Tortilla", Type.WRAP));
+        ingredientMap.put("COTO", new Ingredient("COTO", "Corn Tortilla", Type.WRAP));
+        ingredientMap.put("GRBF", new Ingredient("GRBF", "Ground Beef", Type.PROTEIN));
+        ingredientMap.put("CARN", new Ingredient("CARN", "Carnitas", Type.PROTEIN));
+        ingredientMap.put("TMTO", new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES));
+        ingredientMap.put("LETC", new Ingredient("LETC", "Lettuce", Type.VEGGIES));
+        ingredientMap.put("CHED", new Ingredient("CHED", "Cheddar", Type.CHEESE));
+        ingredientMap.put("JACK", new Ingredient("JACK", "Monterry Jack", Type.CHEESE));
+        ingredientMap.put("SLSA", new Ingredient("SLSA", "Salsa", Type.SAUCE));
+        ingredientMap.put("SRCR", new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
     }
 
     @Override
